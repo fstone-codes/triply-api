1ec15e83470d25837c4cca375649802117d72f92
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-11-20 19:43:26 -0500

    edit and test trip patch api validation

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -5,65 +5,50 @@ const knex = initKnex(configuration);
 // POST api "/api/trips"
 export const addOne = async (req, res) => {
     try {
-        // validate body contents
-        // ensure no empty inputs for trip name + destination
-        if (!req.body.trip_name || !req.body.destination) {
+        const { trip_name, destination, start_date, end_date } = req.body;
+
+        if (!trip_name || !destination) {
             return res.status(400).json({
                 error: "Please provide the name and destination for the trip in the request body",
             });
         }
 
-        // ensure no empty inputs for trip start + end date
-        if (!req.body.start_date || !req.body.end_date) {
+        if (!start_date || !end_date) {
             return res.status(400).json({
                 error: "Please provide the start and end dates for the trip in the request body",
             });
         }
 
-        // ensure valid trip start + end date format
-        // ensure start date is the same or before the end date
         function dateFormat(date) {
             const datePattern = /^\d{4}-?(0[1-9]|1[0-2])-?(0[1-9]|[12]\d|3[01])$/;
             return datePattern.test(date);
         }
 
-        if (
-            !dateFormat(req.body.start_date) ||
-            !dateFormat(req.body.end_date) ||
-            req.body.start_date > req.body.end_date
-        ) {
+        if (!dateFormat(start_date) || !dateFormat(end_date) || start_date > end_date) {
             return res.status(400).json({
                 error: "Please provide valid start and end dates for the trip in the request body",
             });
         }
 
-        // ensure no spaces at the beginning of text inputs
         req.body.trip_name = req.body.trip_name.trim();
         req.body.destination = req.body.destination.trim();
 
-        // filter the "users" table to the first user/row with the "user_id" from the post request body
         const checkUser = await knex("users").where({ id: req.body.user_id }).first();
 
-        // ensure new trip's user exists in "users" table
         if (!checkUser) {
             return res.status(404).json({
                 error: `User id ${req.body.user_id} not found`,
             });
         }
 
-        // insert request body into database ("trips" table)
         const tripFound = await knex("trips").insert(req.body);
 
-        // store newly incremented item/row id (primary key) in a variable
         const tripData = tripFound[0];
 
-        // store entire new table row in a variable
-        // destructure object from array
         const [createdTrip] = await knex("trips").where({
             id: tripData,
         });
 
-        // send response status and body
         res.status(201).json(createdTrip);
     } catch (error) {
         console.error(error);
@@ -76,17 +61,14 @@ export const getAll = async (req, res) => {
     try {
         const { userId } = req.query;
 
-        // ensure "userId" query parameter exists
         if (!userId) {
             return res.status(400).json({
                 error: `Please provide a userId as a query parameter`,
             });
         }
 
-        // filter the "users" table to the first user/row with the "userId" from the query parameter
         const checkUser = await knex("users").where({ id: userId }).first();
 
-        // ensure new trip's user exists in "users" table
         if (!checkUser) {
             return res.status(404).json({
                 error: `User with id ${userId} not found`,
@@ -109,15 +91,14 @@ export const getSingleTrip = async (req, res) => {
 
         const tripFound = await knex("trips").where({ id: tripId });
 
-        // ensure new trip's user exists in "users" table
         if (!tripFound) {
             return res.status(404).json({
                 error: `Trip id ${tripId} not found`,
             });
         }
-        const tripData = tripFound[0];
+        const singleTrip = tripFound[0];
 
-        res.status(200).json(tripData);
+        res.status(200).json(singleTrip);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Unable to retrieve trip" });
@@ -127,6 +108,38 @@ export const getSingleTrip = async (req, res) => {
 // PATCH api "/api/trips/:tripId"
 export const updateOne = async (req, res) => {
     try {
+        /*         const { trip_name, destination, start_date, end_date } = req.body;
+
+        if (trip_name) req.body.trip_name = trip_name.trim();
+        if (destination) req.body.destination = destination.trim();
+
+        if ((trip_name && trip_name.trim() === "") || (destination && destination.trim() === "")) {
+            return res.status(400).json({
+                error: "Please provide the name and destination for the trip in the request body",
+            });
+        }
+
+        // if ((start_date && start_date === "") || (end_date && end_date === "")) {
+        //     return res.status(400).json({
+        //         error: "Please provide the start and end dates for the trip in the request body",
+        //     });
+        // }
+
+        function dateFormat(date) {
+            const datePattern = /^\d{4}-?(0[1-9]|1[0-2])-?(0[1-9]|[12]\d|3[01])$/;
+            return datePattern.test(date);
+        }
+
+        if (
+            (start_date && !dateFormat(start_date)) ||
+            (end_date && !dateFormat(end_date)) ||
+            (start_date && end_date && start_date > end_date)
+        ) {
+            return res.status(400).json({
+                error: "Please provide valid start and end dates for the trip in the request body",
+            });
+        } */
+
         const { tripId } = req.params;
 
         const rowsUpdated = await knex("trips").where({ id: tripId }).update(req.body);
